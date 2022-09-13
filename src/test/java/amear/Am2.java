4e78148390b7171a8f2919b7246216e849cbeb18
pvpatil5 <pvpatil123@yahoo.com>
2022-09-13 16:05:13 +0530

    qweert

--- src/test/java/amear/Am2.java
+++ src/test/java/amear/Am2.java
@@ -3,7 +3,8 @@ package amear;
 public class Am2 {
 
 	public static void main(String[] args) {
-System.out.println(" hi Akash");
+		System.out.println(" hi Akash");
+		System.out.println("hi tyss");
 	}
 
 }
